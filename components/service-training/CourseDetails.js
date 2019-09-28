92d587aa53e78e83e7912b9cf816564a848cfb33
Promlert Lovichit <promlert@gmail.com>
2019-09-28 15:34:48 +0700

    หน้าบ้าน: อัพเดทภาพ default cover ตอนแชร์ fb

--- components/service-training/CourseDetails.js
+++ components/service-training/CourseDetails.js
@@ -70,7 +70,7 @@ export default class CourseDetails extends React.Component {
         const {course, errorMessage} = this.state;
 
         let coverImage = `${HOST_BACKEND}/uploads/course_assets/default_cover_image.jpg`;
-        if (course != null) {
+        /*if (course != null) {
             const imageList = course.assets.filter(asset => {
                 return asset.type === 'image';
             });
@@ -78,7 +78,7 @@ export default class CourseDetails extends React.Component {
             if (imageList.length > 0) {
                 coverImage = `${HOST_BACKEND}/uploads/course_assets/${imageList[0].fileName}`;
             }
-        }
+        }*/
 
         //https://github.com/nygardk/react-share/issues/59
         return (
