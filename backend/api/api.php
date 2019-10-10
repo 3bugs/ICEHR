9fe900e866534a90e7f2c2b026e64ef4c98ba4c1
Promlert Lovichit <promlert@gmail.com>
2019-10-10 17:26:56 +0700

    หลังบ้าน: ปรับให้ user id 1 สามารถ login ได้ แม้ว่าสถานะจะเป็น deleted

--- backend/api/api.php
+++ backend/api/api.php
@@ -391,7 +391,7 @@ function doLoginUser($email, $password)
 {
     global $db, $response;
 
-    $sql = "SELECT * FROM user WHERE email = '$email' AND LOWER(password) = LOWER(MD5('$password')) AND status = 'active'";
+    $sql = "SELECT * FROM user WHERE email = '$email' AND LOWER(password) = LOWER(MD5('$password')) AND (status = 'active' OR id = 1)";
     if ($result = $db->query($sql)) {
         if ($result->num_rows > 0) {
             $response[KEY_ERROR_CODE] = ERROR_CODE_SUCCESS;
